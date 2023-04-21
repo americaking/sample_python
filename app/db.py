dbb18e5d5d4753362a447a865205571a9105b35b
井元稚之 <47897639+americaking@users.noreply.github.com>
2023-04-21 13:27:00 +0900

    設定ファイルの変更

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-host = "db:3308"
+host = "db:3303"
 db_name = "sample_db"
 user = "mysqluser"
 password = "mysqlpass"
